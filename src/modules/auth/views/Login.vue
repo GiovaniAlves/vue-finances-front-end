ad5bb30debc8ac6320b696d7a8fa189dfd7f01bb
giovani.alves <giovani.alves.glv@gmail.com>
2021-11-08 11:50:38 -0300

    Foram insiridas as mensagens de validação de erro nos campos email e senha usando o Vuetify e o Vuelidate

--- src/modules/auth/views/Login.vue
+++ src/modules/auth/views/Login.vue
@@ -4,7 +4,7 @@
       <v-flex
         xs12
         sm6
-        md4
+        md5
         lg4
         xl3
       >
@@ -17,16 +17,22 @@
           <v-card-text>
             <v-form>
               <v-text-field
+                :error-messages="emailErrors"
+                :success="!$v.user.email.$invalid"
                 prepend-icon="email"
                 name="email"
                 label="Email"
                 type="email"
+                v-model.trim="$v.user.email.$model"
               ></v-text-field>
               <v-text-field
+                :error-messages="passwordErrors"
+                :success="!$v.user.password.$invalid"
                 prepend-icon="lock"
                 name="password"
                 label="Password"
                 type="password"
+                v-model.trim="$v.user.password.$model"
               ></v-text-field>
             </v-form>
             <v-btn block depressed>
@@ -36,7 +42,13 @@
 
           <v-card-actions>
             <v-spacer></v-spacer>
-            <v-btn color="primary" large>Login</v-btn>
+            <v-btn
+              color="primary"
+              large
+              :disabled="$v.$invalid"
+              @click="submit">
+              Login
+            </v-btn>
           </v-card-actions>
 
         </v-card>
@@ -46,7 +58,53 @@
 </template>
 
 <script>
+
+import { email, minLength, required } from 'vuelidate/lib/validators'
+
 export default {
-  name: 'Login'
+  name: 'Login',
+  data () {
+    return {
+      user: {
+        email: '',
+        password: ''
+      }
+    }
+  },
+  computed: {
+    emailErrors () {
+      const errors = []
+      const email = this.$v.user.email
+      if (!email.$dirty) { return errors }
+      !email.email && errors.push('Insira um e-mail válido')
+      !email.required && errors.push('Esse campo é obrigatório')
+      return errors
+    },
+    passwordErrors () {
+      const errors = []
+      const password = this.$v.user.password
+      if (!password.$dirty) { return errors }
+      !password.required && errors.push('Esse campo é obrigatório')
+      !password.minLength && errors.push(`Insira no mínimo ${password.$params.minLength.min} caracteres`)
+      return errors
+    }
+  },
+  validations: {
+    user: {
+      email: {
+        required,
+        email
+      },
+      password: {
+        required,
+        minLength: minLength(6)
+      }
+    }
+  },
+  methods: {
+    submit () {
+      console.log('Vuelidade: ', this.$v)
+    }
+  }
 }
 </script>
