c668c62a5ed87d2526b41a5bb0ce9a5d73bc0c27
giovani.alves <giovani.alves.glv@gmail.com>
2021-11-11 15:03:20 -0300

    Usei a guarde de rota beforeEach para proteger as rotas que necessitam de autenticação

--- src/modules/auth/services/auth-service.js
+++ src/modules/auth/services/auth-service.js
@@ -1,6 +1,7 @@
 import apollo, { onLogin } from '@/plugins/apollo'
 import LoginMutation from './../graphql/Login.gql'
 import SignUpMutation from './../graphql/SignUp.gql'
+import UserQuery from './../graphql/User.gql'
 
 const login = async ({ email, password }) => {
   const response = await apollo.mutate({
@@ -29,7 +30,16 @@ const signup = async ({ email, password, name }) => {
   return signup
 }
 
+const user = async (options = {}) => {
+  const response = await apollo.query({
+    query: UserQuery,
+    ...options
+  })
+  return response.data.user
+}
+
 export default {
   login,
-  signup
+  signup,
+  user
 }
--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,8 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
+
+import { AUTH_TOKEN } from '@/plugins/apollo'
+import AuthService from '@/modules/auth/services/auth-service'
 import authRoutes from '@/modules/auth/router'
 import dashboardRoutes from '@/modules/dashboard/router'
 
@@ -8,13 +11,37 @@ Vue.use(VueRouter)
 const routes = [
   ...authRoutes,
   ...dashboardRoutes,
-  { path: '', redirect: '/login' }
+  {
+    path: '',
+    redirect: '/login'
+  }
 ]
 
 const router = new VueRouter({
   mode: 'history',
   base: process.env.BASE_URL,
   routes
+})
+
+router.beforeEach(async (to, from, next) => {
+  if (to.matched.some(route => route.meta.requiresAuth)) {
+    const token = window.localStorage.getItem(AUTH_TOKEN)
+    const loginRoute = {
+      path: '/login',
+      query: { redirect: to.fullPath }
+    }
+    if (token) {
+      try {
+        await AuthService.user({ fetchPolicy: 'network-only' })
+        return next()
+      } catch (e) {
+        console.log('Auth login error: ', e)
+        return next(loginRoute)
+      }
+    }
+    return next(loginRoute)
+  }
+  next()
 })
 
 export default router
