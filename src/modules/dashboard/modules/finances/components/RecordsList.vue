73abdf69b200db56a063d97bec0d7192b344bc0f
giovani.alves <giovani.alves.glv@gmail.com>
2021-11-17 15:56:21 -0300

    Os lançamentos estão dentro de um v-card agora e foi adicionado o sub total dos lancamentos no footer do card

--- src/modules/dashboard/modules/finances/components/RecordsList.vue
+++ src/modules/dashboard/modules/finances/components/RecordsList.vue
@@ -1,19 +1,35 @@
 <template>
-  <v-list two-line subheader>
-    <template v-for="(groupedRec, dateKey, index) in groupedRecords">
-      <v-subheader :key="dateKey">{{ dateKey }}</v-subheader>
-      <RecordsListItem
-        v-for="record in groupedRec"
-        :key="record.id"
-        :record="record"/>
-      <v-divider :key="`${dateKey}-${index}`" v-if="showDivider(index, groupedRecords)"></v-divider>
-    </template>
-  </v-list>
+  <v-card>
+
+    <v-list two-line subheader>
+      <template v-for="(groupedRec, dateKey, index) in groupedRecords">
+        <v-subheader :key="dateKey">{{ dateKey }}</v-subheader>
+        <RecordsListItem
+          v-for="record in groupedRec"
+          :key="record.id"
+          :record="record"/>
+        <v-divider :key="`${dateKey}-${index}`" v-if="showDivider(index, groupedRecords)"></v-divider>
+      </template>
+    </v-list>
+
+    <v-footer class="pa-2">
+      <v-flex text-xs-right>
+        <h4 class="font-weight-light">
+          <span>Saldo do mês</span>
+          <strong class="ml-5" :class="amountColor(totalAmount)">{{ formatCurrency(totalAmount) }}</strong>
+        </h4>
+      </v-flex>
+    </v-footer>
+  </v-card>
 </template>
 
 <script>
 
 import { groupBy } from '@/utils'
+
+import amountColorMixin from './../mixins/amount-color'
+import formatCurrencyMixin from '@/mixins/format-currency'
+
 import moment from 'moment'
 import RecordsListItem from './RecordsListItem'
 import RecordsService from './../services/records-service'
@@ -21,6 +37,10 @@ import RecordsService from './../services/records-service'
 export default {
   name: 'RecordsList',
   components: { RecordsListItem },
+  mixins: [
+    amountColorMixin,
+    formatCurrencyMixin
+  ],
   data () {
     return {
       records: []
@@ -31,6 +51,9 @@ export default {
       return groupBy(this.records, 'date', (record, datekey) => {
         return moment(record[datekey]).format('DD/MM/YYYY')
       })
+    },
+    totalAmount () {
+      return this.records.reduce((sum, record) => sum + record.amount, 0)
     }
   },
   async created () {
