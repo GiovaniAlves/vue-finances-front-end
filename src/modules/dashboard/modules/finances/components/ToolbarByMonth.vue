50d11f2562d79efd60db99287d822b25d60f2cb0
giovani.alves <giovani.alves.glv@gmail.com>
2021-11-18 10:46:04 -0300

    Listando os lançamentos de acordo com o mês selecionado

--- src/modules/dashboard/modules/finances/components/ToolbarByMonth.vue
+++ src/modules/dashboard/modules/finances/components/ToolbarByMonth.vue
@@ -4,21 +4,25 @@
 
       <v-flex xs1>
         <div class="text-xs-left">
-          <v-btn icon>
+          <v-btn
+            icon
+            @click="decrement">
             <v-icon>chevron_left</v-icon>
           </v-btn>
         </div>
       </v-flex>
 
       <v-flex xs10>
-          <v-toolbar-title class="flex text-center">
-            <span>Novembro 2020</span>
-          </v-toolbar-title>
+        <v-toolbar-title class="flex text-center">
+          <span>{{ currentMonth }}</span>
+        </v-toolbar-title>
       </v-flex>
 
       <v-flex xs1>
         <div class="text-xs-right">
-          <v-btn icon>
+          <v-btn
+            icon
+            @click="increment">
             <v-icon>chevron_right</v-icon>
           </v-btn>
         </div>
@@ -29,11 +33,39 @@
 </template>
 
 <script>
+
+import moment from 'moment'
+
 export default {
-  name: 'ToolbarByMonth'
+  name: 'ToolbarByMonth',
+  props: {
+    format: String
+  },
+  data () {
+    return {
+      date: moment()
+    }
+  },
+  computed: {
+    currentMonth () {
+      return this.date.format('MMMM YYYY') // Pegando o mês e o ano de forma completa
+    }
+  },
+  created () {
+    this.emit()
+  },
+  methods: {
+    emit () {
+      this.$emit('month', this.date.format(this.format))
+    },
+    decrement () {
+      this.date = this.date.clone().subtract(1, 'month')
+      this.emit()
+    },
+    increment () {
+      this.date = this.date.clone().add(1, 'month')
+      this.emit()
+    }
+  }
 }
 </script>
-
-<style scoped>
-
-</style>
